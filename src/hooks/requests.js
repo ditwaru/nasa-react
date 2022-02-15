bcd092997d837d9e843b7860b6fa20579c62bb2e
Daniel Itwaru <daniel.itwaru@elevationchurch.org>
2022-02-15 02:53:09 -0500

    initial commit

---
+++ src/hooks/requests.js
@@ -0,0 +1,53 @@
+const API_URL = process.env.REACT_APP_API_URL;
+
+// Load planets and return as JSON.
+async function httpGetPlanets() {
+  try {
+    const response = await fetch(`${API_URL}/planets`);
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// Load launches, sort by flight number, and return as JSON.
+async function httpGetLaunches() {
+  try {
+    const response = await fetch(`${API_URL}/launches`);
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// Submit given launch data to launch system.
+async function httpSubmitLaunch(launch) {
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(launch),
+    });
+  } catch (err) {
+    return {
+      ok: false,
+    };
+  }
+}
+
+// Delete launch with given ID.
+async function httpAbortLaunch(id) {
+  try {
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: 'DELETE',
+    });
+  } catch (err) {
+    return {
+      ok: false,
+    };
+  }
+}
+
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
